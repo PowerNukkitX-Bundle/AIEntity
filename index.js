335df7cf88fe8b53b8beae431ef2f3dcdb00a3c4
超神的冰凉 <2801621420@qq.com>
2022-08-25 21:00:05 +0800

    初始化提交

--- index.js
+++ index.js
@@ -1,28 +1,115 @@
-/*
- * 整个JS代码将在服务器刚启动时加载，请务必牢记：大部分与游戏相关的内容，
- * 如监听事件，生成物品，操作地图，控制生物等等都不能在刚开始的时候进行！
- * 
- * The entire JS code will be loaded when the server is just started. 
- * Please keep in mind that most game related content, such as monitoring events, 
- * generating items, operating worlds, controlling entities, etc., cannot be carried out at the beginning!
- */
-console.log("Wow world")
+import { EventPriority, PowerNukkitX as pnx } from ":powernukkitx";
+import { JavaClassBuilder } from ":jvm";
+import { EntityIntelligent } from "cn.nukkit.entity.EntityIntelligent";
+import { Entity } from "cn.nukkit.entity.Entity";
+import { Server } from "cn.nukkit.Server";
+import { BehaviorGroup } from "cn.nukkit.entity.ai.behaviorgroup.BehaviorGroup";
+import { WalkController } from "cn.nukkit.entity.ai.controller.WalkController";
+import { LookController } from "cn.nukkit.entity.ai.controller.LookController";
+import { SimpleFlatAStarRouteFinder } from "cn.nukkit.entity.ai.route.SimpleFlatAStarRouteFinder";
+import { WalkingPosEvaluator } from "cn.nukkit.entity.ai.route.posevaluator.WalkingPosEvaluator";
+import { IBehaviorGroup } from "cn.nukkit.entity.ai.behaviorgroup.IBehaviorGroup";
+import { Collections } from "java.util.Collections";
+import { Set as JSet } from "java.util.Set"
 
-/*
- * main函数将会在PNX服务器初始化完成后被调用，此函数被调用时及以后您可以进行与游戏相关的操作。
-
- * The main function will be called after the PNX server is initialized. 
- * When this function is called and later, you can perform game related operations.
+/**
+ * @class
  */
+const MySheep = new JavaClassBuilder("MySheep", EntityIntelligent)
+    .setJSDelegate({
+        /** @type {Map<MySheep, cn.nukkit.entity.ai.behaviorgroup.BehaviorGroup>} */
+        behaviorGroups: new Map(),
+        new(chunk, nbt) {
+            console.log(chunk);
+            console.log(nbt);
+            return [chunk, nbt];
+        },
+        /**
+         * @arg javaThis {cn.nukkit.entity.EntityIntelligent}
+         */
+        constructor(javaThis) {
+            javaThis.setEnablePitch(true);
+            javaThis.setNameTagAlwaysVisible(true);
+            javaThis.setNameTagVisible(true);
+            javaThis.setNameTag("PNX JS Sheep");
+            javaThis.spawnToAll();
+        },
+        getNetworkId() {
+            return 13;
+        },
+        getWidth() {
+            return 0.9;
+        },
+        getHeight() {
+            return 1.3;
+        },
+        /**
+         * @arg javaThis {cn.nukkit.entity.EntityIntelligent}
+         */
+        onUpdate(javaThis, currentTick) {
+            const ret = javaThis.__super__onUpdate(currentTick);
+            if ((currentTick & 7) === 0) { // currentTick % 8 === 0
+                let nearestPlayer = null;
+                let nearestDis = 99999999999;
+                for (const i of javaThis.getLevel().getPlayers().values()) {
+                    /** @type {cn.nukkit.Player} */
+                    const player = i;
+                    const tmpDis = player.distanceSquared(javaThis);
+                    if (tmpDis < nearestDis) {
+                        nearestPlayer = player;
+                        nearestDis = tmpDis;
+                    }
+                }
+                if (nearestPlayer != null) {
+                    javaThis.lookTarget = nearestPlayer; // Look towards the player.
+                    javaThis.moveTarget = nearestPlayer; // Walk to the player.
+                }
+            }
+            return ret;
+        },
+        getBehaviorGroup(javaThis) {
+            const tmp = this.behaviorGroups.get(javaThis);
+            if (tmp) {
+                return tmp;
+            } else {
+                this.behaviorGroups.set(javaThis, new BehaviorGroup(javaThis.tickSpread, Collections.emptySet(), Collections.emptySet(), Collections.emptySet(), JSet.of(
+                    new WalkController(), new LookController(true, true)
+                ), new SimpleFlatAStarRouteFinder(new WalkingPosEvaluator(), javaThis)));
+                return this.behaviorGroups.get(javaThis);
+            }
+        },
+        close(javaThis) {
+            this.behaviorGroups.delete(javaThis);
+            javaThis.__super__close();
+        }
+    }).addJavaConstructor("new", "constructor", ["cn.nukkit.level.format.FullChunk", "cn.nukkit.nbt.tag.CompoundTag"], "cn.nukkit.level.format.FullChunk", "cn.nukkit.nbt.tag.CompoundTag")
+    .addJavaMethod("getNetworkId", "getNetworkId", "int")
+    .addJavaMethod("getWidth", "getWidth", "float")
+    .addJavaMethod("getHeight", "getHeight", "float")
+    .addJavaSuperMethod("onUpdate", "boolean", "int")
+    .addJavaMethod("onUpdate", "onUpdate", "boolean", "int")
+    .addJavaMethod("getBehaviorGroup", "getBehaviorGroup", IBehaviorGroup)
+    .addJavaSuperMethod("close", "void")
+    .addJavaMethod("close", "close", "void")
+    .compileToJavaClass();
+
 export function main() {
-    console.warn("Hello world")
+    pnx.listenEvent("cn.nukkit.event.player.PlayerInteractEvent", EventPriority.NORMAL, e => {
+        if (e.getItem().getNamespaceId() === "minecraft:stick") {
+            const level = e.getPlayer().getLevel();
+            level.addEntity(new MySheep(level.getChunk(e.getPlayer().getChunkX(), e.getPlayer().getChunkZ()), Entity.getDefaultNBT(e.getPlayer())));
+        }
+    })
 }
 
-/*
- * close函数将会在在PNX服务器关闭时或您的插件被卸载时调用，此时您应当进行一些清理收尾工作。
- * The close function will be called when the PNX server is shutdown or your plugin is uninstalled. 
- * At this time, you should do some cleaning and finishing work.
- */
 export function close() {
-    console.error("Goodbye world")
+    for (const i of Server.getInstance().getLevels().values()) { // Clear all the JS entities.
+        /** @type {cn.nukkit.level.Level} */
+        const level = i;
+        level.getEntities().forEach(e => {
+            if (e instanceof MySheep) {
+                e.close();
+            }
+        })
+    }
 }
